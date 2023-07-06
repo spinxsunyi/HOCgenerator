995b90f058c62b08ef22f8f4e39335ff34b2414b
spinxsunyi <just_eka@ymail.com>
2023-07-06 19:10:13 +0800

    initial commit

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const mongoString = process.env.DATABASE_url
+
+// connect to database
+const database = mongoose.connection
+database.on('error', (error) => {
+    console.log(error)
+})
+database.once('connected', () => {
+    console.log('connected database');
+})
+
+// Declare app by express
+const app = express();
+app.use(express.json());
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+    extended: true
+})); 
+
+// Set EJS as view engine
+app.set('view engine', 'ejs');
+
+// Importing all required routes
+const HOCRoute = require('./routes/HOCRoute')
+
+app.use('/', HOCRoute);
+// ?? findout how the app.use is working?
+
+app.listen(3001, ()=>{
+    console.log(`server started at 3001`)
+})
